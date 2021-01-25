bdebcb8313a8735395fc48a90b47f531cb6c8ad5
Harris <harric17@nala.roche.com>
2021-01-24 17:14:22 -0800

    comments

--- day04/py/part1.py
+++ day04/py/part1.py
@@ -2,14 +2,17 @@ import hashlib
 
 secretkey="ckczppom"
 #secretkey="abcdef"
+#secretkey="pqrstuv"
+
 cnt=0
 stoploop=False
 while stoploop==False:
+	cnt+=1
 	mystring=secretkey+str(cnt)
 	myhash=hashlib.md5(mystring.encode("utf")).hexdigest()
 	if myhash[0:5]=="00000":
 		stoploop=True
-	cnt+=1
-	
+
 print(mystring)
 print(myhash)
+print("Final Answer:",cnt)
