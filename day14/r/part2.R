87d827c30352508fbdd6a1a6e46970538c3933c6
Harris <harric17@nala.roche.com>
2021-02-11 17:25:23 -0800

    p2 gif

--- day14/r/part2.R
+++ day14/r/part2.R
@@ -44,4 +44,59 @@ dflong= dflong %>%
 
 table(dflong$winner)#part 2 solution
 
-#need to make a growing barchart here
+###
+# Animated barchart
+###
+
+getpoints=function(reindeer){#This function gets each reindeer's current points at every timepoint
+  points=c()
+  totalpoints=0
+  for(i in 1:nrow(dflong)){
+    if(!is.na(dflong$winner[i]) & dflong$winner[i]==reindeer){
+      totalpoints=totalpoints+1
+    }
+    if(dflong$name[i]==reindeer){
+    points[i]=totalpoints
+    }
+    else{
+      points[i]=NA
+    }
+  }
+  return(points)
+}
+
+mylist=list()#iterate over each reindeer and combine them into a list
+for(name in unique(dflong$name)){
+  mylist[[name]]=getpoints(name)
+}
+
+#combine all point status into a single variable
+#I am a bad person for doing combining things this way
+dflong$points=ifelse(!is.na(mylist[[1]]),mylist[[1]],
+       ifelse(!is.na(mylist[[2]]),mylist[[2]],
+              ifelse(!is.na(mylist[[3]]),mylist[[3]],
+                     ifelse(!is.na(mylist[[4]]),mylist[[4]],
+                            ifelse(!is.na(mylist[[5]]),mylist[[5]],
+                                   ifelse(!is.na(mylist[[6]]),mylist[[6]],
+                                          ifelse(!is.na(mylist[[7]]),mylist[[7]],
+                                                 ifelse(!is.na(mylist[[8]]),mylist[[8]],
+                                                        ifelse(!is.na(mylist[[9]]),mylist[[9]],
+                                                               NA)))))))))
+
+
+p=ggplot(dflong, aes(fill=name, y=points, x=name)) + 
+  geom_bar(position="dodge", stat="identity")+
+  theme(
+        axis.title.x=element_blank(),
+        legend.title = element_blank()
+  ) +
+  transition_time(order)
+
+animate(p
+        # ,nframes=(nrow(allpositions)/2)+10
+        ,nframe=130
+        # ,fps=100
+        ,end_pause = 30
+        ,renderer=gifski_renderer("part2.gif")
+)
+
